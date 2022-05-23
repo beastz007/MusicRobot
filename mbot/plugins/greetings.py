fa6c4ac9ae1f53e3efa4447ee84c9f96a353c1df
beastz007 <104611392+beastz007@users.noreply.github.com>
2022-05-23 16:16:44 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -32,15 +32,15 @@ from os import execvp,sys
 async def start(client,message):
     reply_markup = [[
         InlineKeyboardButton(
-            text="Bot Channel", url="https://t.me/TheCrowClub"),
+            text="Bot Channel", url="https://t.me/BeastXBots"),
         InlineKeyboardButton(
-            text="Repo",
-            url="https://github.com/rozari0/NeedMusicRobot"),
+            text="Support",
+            url="https://t.me/BeastXBotz"),
         InlineKeyboardButton(text="Help",callback_data="helphome")
         ],
         [
             InlineKeyboardButton(text="Donate",
-            url="https://www.buymeacoffee.com/rozario"),
+            url="https://www.buymeacoffee.com/B34STX"),
         ]]
     if LOG_GROUP:
 
@@ -84,7 +84,7 @@ async def help(_,message):
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
 
-    await message.reply_text(f"Hello **{message.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
+    await message.reply_text(f"Hello **{message.from_user.first_name}**, I'm **@LuciferMusicBot**.\nI'm Here to download your music.",
                         reply_markup=InlineKeyboardMarkup(button))
 
 @Mbot.on_callback_query(filters.regex(r"help_(.*?)"))
@@ -99,5 +99,5 @@ async def help_home(_,query):
     button = [
         [InlineKeyboardButton(text=i, callback_data=f"help_{i}")] for i in HELP
     ]
-    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@NeedMusicRobot**.\nI'm Here to download your music.",
+    await query.message.edit(f"Hello **{query.from_user.first_name}**, I'm **@LuciferMusicBot**.\nI'm Here to download your music.",
                         reply_markup=InlineKeyboardMarkup(button))
